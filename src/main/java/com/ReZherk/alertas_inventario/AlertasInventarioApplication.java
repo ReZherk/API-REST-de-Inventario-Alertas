8cf09d7d0ac5157789a55d20f2225a8b9e6cd789
ReZherk <patrickcomeresbueno@gmail.com>
2025-08-19 21:04:51 -0500

    First upload to git

---
+++ src/main/java/com/ReZherk/alertas_inventario/AlertasInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.ReZherk.alertas_inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertasInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertasInventarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ReZherk/alertas_inventario/AlertasInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ReZherk.alertas_inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertasInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
