e3388fff5ce7ea20a08dddc2d619be89e3d67cfc
ReZherk <patrickcomeresbueno@gmail.com>
2025-09-04 10:58:22 -0500

    The alert entity was created

---
+++ src/main/java/com/ReZherk/alertas_inventario/domain/model/Alert.java
@@ -0,0 +1,48 @@
+package com.ReZherk.alertas_inventario.domain.model;
+
+
+import com.ReZherk.alertas_inventario.domain.model.enums.AlertType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "alerts")
+public class Alert {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private Product product;
+
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private AlertType type;
+
+    @Column(nullable = false, length = 200)
+    private String message;
+
+
+    @Column(nullable = false)
+    private Boolean acknowledged = false;
+
+
+    @Column(nullable = false)
+    private LocalDateTime createAt;
+
+    @PrePersist
+    public void prePersist() {
+        createAt = LocalDateTime.now();
+    }
+
+}
